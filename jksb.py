8b4c9d57f623f3e97ccef7dfd41d229e2b6749c9
leeyiding <1843272640@qq.com>
2021-03-03 18:54:34 +0800

    优化代码逻辑

--- jksb.py
+++ jksb.py
@@ -12,27 +12,9 @@ import logging
 import json
 from bs4 import BeautifulSoup
 
-# 定义环境变量
-rootDir = os.path.dirname(os.path.abspath(__file__))
-configPath = rootDir + "/config.json"
-logDir = rootDir + "/logs"
-
-# 日志输出控制台
-global logger
-logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-# 日志输入文件
-date = time.strftime("%Y-%m-%d", time.localtime()) 
-if not os.path.exists(logDir):
-    logger.warning("未检测到日志目录存在，开始创建logs目录")
-    os.mkdir(logDir)
-fh = logging.FileHandler('{}/{}.log'.format(logDir,date), mode='a', encoding='utf-8')
-fh.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
-logger.addHandler(fh)
-
 class ZZUjksb(object):
     # 初始化
-    def __init__(self,user):
+    def __init__(self,user,logger):
         self.username = user['username']
         self.password = user['password']
         self.data1 = user['data1']
@@ -70,7 +52,7 @@ class ZZUjksb(object):
         try:
             r = requests.post(url=loginUrl,data=data,headers=headers,timeout=300)
         except Exception as e:
-            self.logger.error("登陆失败，请检查网络后重试")
+            self.logger.error("\t登陆失败，请检查网络后重试")
             self.logger.error(e)
             return False
         else:
@@ -78,10 +60,10 @@ class ZZUjksb(object):
             bs = BeautifulSoup(r.text,"html.parser")
             errmsg = bs.find('div',style="width:296px;height:100%;font-size:14px;color:#333;line-height:26px;float:left;")
             if errmsg:
-                self.logger.error(errmsg.text)
+                self.logger.error("\t" + errmsg.text.strip())
                 return False
             else:
-                self.logger.info("登陆成功")
+                self.logger.info("\t登陆成功")
                 self.ptopid = re.findall(r"[a-zA-Z0-9]{33}",r.text)[0]
                 self.sid = re.findall(r"[0-9]{18}",r.text)[0]
                 self.data1["ptopid"] = self.ptopid
@@ -100,15 +82,15 @@ class ZZUjksb(object):
         try:
             r = requests.get(url=checkUrl,params=paramas)
         except:
-            self.logger.error("检查打卡状态失败")
+            self.logger.error("\t检查打卡状态失败")
         else:
             r.encoding = "utf-8"
             result = re.findall(r"\u4eca\u65e5\u60a8\u5df2\u7ecf\u586b\u62a5\u8fc7\u4e86",r.text)
             if result:
-                self.logger.info(result[0])
+                self.logger.info("\t" + result[0])
                 return True
             else:
-                self.logger.info("检测到今日未打卡，开始打卡")
+                self.logger.info("\t检测到今日未打卡，开始打卡")
                 return False
      
     # 发送消息通知
@@ -123,9 +105,9 @@ class ZZUjksb(object):
             r.encoding = "utf-8"
             r = json.loads(r.text)
             if r['errno'] == 0 :
-                self.logger.info("Server酱通发送知信息成功！")
+                self.logger.info("\tServer酱通发送知信息成功！")
             else:
-                self.logger.error("Server酱通发送知信息失败！")
+                self.logger.error("\tServer酱通发送知信息失败！")
         if notify['ddtoken']:
             webhook = "https://oapi.dingtalk.com/robot/send?access_token={}".format(notify['ddtoken'])
             data = {
@@ -139,11 +121,11 @@ class ZZUjksb(object):
             r.encoding = "utf-8"
             r = json.loads(r.text)
             if r['errcode'] == 0:
-                self.logger.info("钉钉通发送知信息成功！")
+                self.logger.info("\t钉钉通发送知信息成功！")
             elif r['errcode'] == 310000:
-                self.logger.error("钉钉通发送知信息失败!请为钉钉机器人添加关键词'健康上报'")
+                self.logger.error("\t钉钉通发送知信息失败!请为钉钉机器人添加关键词'健康上报'")
             else:
-                self.logger.error("钉钉通发送知信息失败!")
+                self.logger.error("\t钉钉通发送知信息失败!")
 
     # 签到
     def checkin(self):
@@ -172,21 +154,28 @@ class ZZUjksb(object):
         r.encoding = "utf-8"
         bs = BeautifulSoup(r.text,"html.parser")
         msg = bs.find('div',style="width:296px;height:100%;font-size:14px;color:#333;line-height:26px;float:left;")
-        self.logger.info(msg.text)
-        self.sendMsg(self.notify,msg.text)
+        if msg:
+            self.logger.info("\t" + msg.text)
+            self.sendMsg(self.notify,msg.text)
+            return True
+        else:
+            return False
         
     def main(self):
-        for i in range(1,5):
-            if i < 4:
-                self.logger.info("正在第{}次模拟登陆健康上报系统".format(i))
-                if self.login():
-                    if self.checkStatus():
-                        self.logger.info("无需重复打卡，程序执行完成！")
+        for i in range(1,4):
+            self.logger.info("\t正在第{}次模拟登陆健康上报系统".format(i))
+            # 判断登录状态
+            if self.login():
+                # 判断打卡状态
+                if self.checkStatus():
+                    self.logger.info("\t无需重复打卡，程序执行完成！")
+                    break
+                else:
+                    # 判断签到状态
+                    if self.checkin():
                         break
-                    else:
-                        self.checkin()
-            else:
-                self.logger.error("已连续尝试模拟登陆三次失败，请检查网络后重试！")
+        if i == 3:
+            self.logger.error("\t已连续尝试模拟登陆三次失败，请检查网络或报错信息后重试！")
 
 def readJson(configPath):
     if os.path.exists(configPath):
@@ -195,12 +184,34 @@ def readJson(configPath):
             users = json.load(fp)
             return users
     else:
-        logger.error("未检测到配置文件config.json存在，请按照README.md说明创建配置文件")
+        logger.error("\t未检测到配置文件config.json存在，请按照README.md说明创建配置文件")
+        logger.info("\t" + "*"*20 + "程序运行结束" + "*"*20)
         sys.exit(0)
 
 if __name__ == '__main__':
+    # 定义环境变量
+    rootDir = os.path.dirname(os.path.abspath(__file__))
+    configPath = rootDir + "/config.json"
+    logDir = rootDir + "/logs"
+
+    # 日志输出控制台
+    logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+    # 日志输入文件
+    date = time.strftime("%Y-%m-%d", time.localtime()) 
+    if not os.path.exists(logDir):
+        logger.warning("\t未检测到日志目录存在，开始创建logs目录")
+        os.mkdir(logDir)
+    fh = logging.FileHandler('{}/{}.log'.format(logDir,date), mode='a', encoding='utf-8')
+    fh.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+    logger.addHandler(fh)
+
+    # 运行主程序
+    logger.info("\t" + "*"*20 + "程序运行开始" + "*"*20)
     users = readJson(configPath)
     for count in range(len(users)):
-        user = ZZUjksb(users[count])
+        logging.info("\t开始为用户{}：{}执行健康上报".format(count+1,users[count]['username']))
+        user = ZZUjksb(users[count],logger)
         user.main()
+    logger.info("\t" + "*"*20 + "程序运行结束" + "*"*20)
         
